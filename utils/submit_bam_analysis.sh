348fd73b00a41c682ce7a682520049871f7d8326
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2018-08-31 10:09:47 +0200

    Redesign handling of analysis steps.
    Analysis steps are now provided in a csv file.

--- utils/submit_bam_analysis.sh
+++ utils/submit_bam_analysis.sh
@@ -11,22 +11,15 @@ print_desc()
 usage()
 {
     echo "submit_bam_analysis       -r <string> -n <string> -t <string> -o <string>"
-    echo "                          -a <int> [-c <int>] [-m <int>]"
+    echo "                          -s <string>"
     echo "                          [-debug] [--help]"
     echo ""
     echo "-r <string>               File with reference genome."
     echo "-n <string>               File with normal bam."
     echo "-t <string>               File with tumor bam."
     echo "-o <string>               Output directory."
-    echo "-a <int>                  Analysis type:"
-    echo "                           1 -> Basic analysis (Manta, Strelka2 and MSIsensor)"
-    echo "                           2 -> Complementary analysis (Platypus, CNVkit)"
-    echo "-c <int>                  Number of CPUs used to execute each analysis"
-    echo "                          (default: 1)."
-    echo "                          NOTE: only 1 CPU will be used for those analyses not"
-    echo "                                allowing more than one thread."
-    echo "-m <int>                  Memory in MBs used to execute the different analyses"
-    echo "                          (default: 1024MB)."
+    echo "-s <string>               File with steps to be performed. Expected format:"
+    echo "                          <stepname> <cpus> <mem> <time>"
     echo "-debug                    After ending, do not delete temporary files"
     echo "                          (for debugging purposes)."
     echo "--help                    Display this help and exit."
@@ -110,11 +103,12 @@ launch()
     local_file=$1
     local_cpus=$2
     local_mem=$3
+    local_time=$4
     
     if [ -z "${SBATCH}" ]; then
         $local_file
     else
-        $SBATCH --cpus-per-task=${local_cpus} --mem=${local_mem} $local_file
+        $SBATCH --cpus-per-task=${local_cpus} --mem=${local_mem} --time ${local_time} $local_file
     fi
 }
 
@@ -125,11 +119,7 @@ read_pars()
     n_given=0
     t_given=0
     o_given=0
-    a_given=0
-    c_given=0
-    cpus=1
-    m_given=0
-    mem=1024
+    s_given=0
     debug=0
     while [ $# -ne 0 ]; do
         case $1 in
@@ -163,22 +153,10 @@ read_pars()
                       o_given=1
                   fi
                   ;;
-            "-a") shift
+            "-s") shift
                   if [ $# -ne 0 ]; then
-                      atype=$1
-                      a_given=1
-                  fi
-                  ;;
-            "-c") shift
-                  if [ $# -ne 0 ]; then
-                      cpus=$1
-                      c_given=1
-                  fi
-                  ;;
-            "-m") shift
-                  if [ $# -ne 0 ]; then
-                      mem=$1
-                      m_given=1
+                      sfile=$1
+                      s_given=1
                   fi
                   ;;
             "-debug") debug=1
@@ -231,9 +209,14 @@ check_pars()
         fi
     fi
 
-    if [ ${a_given} -eq 0 ]; then   
-        echo "Error! -a parameter not given!" >&2
+    if [ ${s_given} -eq 0 ]; then   
+        echo "Error! -s parameter not given!" >&2
         exit 1
+    else
+        if [ ! -f ${sfile} ]; then
+            echo "Error! file ${sfile} does not exist" >&2
+            exit 1
+        fi
     fi
 }
 
@@ -379,6 +362,7 @@ execute_step()
     local_stepname=$1
     local_cpus=$2
     local_mem=$3
+    local_time=$4
 
     # Execute step
     create_script ${outd}/scripts/execute_${local_stepname} execute_${local_stepname}
@@ -386,47 +370,62 @@ execute_step()
     echo "STEP: ${local_stepname} ; STATUS: ${status}" >&2
     if [ "$status" != "FINISHED" ]; then
         reset_outdir_for_step ${local_stepname} || exit 1
-        launch ${outd}/scripts/execute_${local_stepname} ${local_cpus} ${local_mem}
+        launch ${outd}/scripts/execute_${local_stepname} ${local_cpus} ${local_mem} ${local_time}
     fi
 }
 
 ########
-perform_basic_analysis()
+entry_is_ok()
 {
-    # Execute Manta
-    stepname="manta"
-    execute_step ${stepname} ${cpus} ${mem}
-
-    # Execute Strelka
-    stepname="strelka"
-    execute_step ${stepname} ${cpus} ${mem}
-
-    # Execute MSIsensor
-    stepname="msisensor"
-    execute_step ${stepname} ${cpus} ${mem}
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{if(NF>=4) print"yes\n"; else print"no\n"}'
 }
 
 ########
-perform_compl_analysis()
+extract_stepname_from_entry()
 {
-    # Execute Platypus for germline sample
-    stepname="platypus_germline"
-    execute_step ${stepname} ${cpus} ${mem}
-
-    # Execute CNVkit
-    stepname="cnvkit"
-    execute_step ${stepname} ${cpus} ${mem}
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $1}'
 }
 
 ########
-process_pars()
+extract_cpus_from_entry()
 {
-    case ${atype} in
-        "1") perform_basic_analysis
-             ;;
-        "2") perform_compl_analysis
-             ;;
-    esac
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $2}'
+}
+
+########
+extract_mem_from_entry()
+{
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $3}'
+}
+
+########
+extract_time_from_entry()
+{
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $4}'
+}
+
+########
+process_sfile()
+{
+    # Read information about the steps to be executed
+    while read entry; do
+        entry_ok=`entry_is_ok "$entry"`
+        if [ ${entry_ok} = "yes" ]; then
+            # Extract entry information
+            stepname=`extract_stepname_from_entry "$entry"`
+            cpus=`extract_cpus_from_entry "$entry"`
+            mem=`extract_mem_from_entry "$entry"`
+            time=`extract_time_from_entry "$entry"`
+
+            # Execute step
+            execute_step ${stepname} ${cpus} ${mem} ${time}
+        fi
+    done < ${sfile}
 }
 
 ########
@@ -444,4 +443,4 @@ create_dirs || exit 1
 
 BASH_SHEBANG=`init_bash_shebang_var`
 
-process_pars
+process_sfile

ea1e617b93c03de700eb86c8433da38149f358c5
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2018-08-31 16:24:47 +0200

    Create bash library file.

---
+++ utils/bam_utils_lib.sh
@@ -0,0 +1,185 @@
+# *- bash -*
+
+########
+init_bash_shebang_var()
+{
+    echo "#!${BASH}"
+}
+
+########
+exclude_readonly_vars()
+{
+    $AWK -F "=" 'BEGIN{
+                         readonlyvars["BASHOPTS"]=1
+                         readonlyvars["BASH_VERSINFO"]=1
+                         readonlyvars["EUID"]=1
+                         readonlyvars["PPID"]=1
+                         readonlyvars["SHELLOPTS"]=1
+                         readonlyvars["UID"]=1
+                        }
+                        {
+                         if(!($1 in readonlyvars)) printf"%s\n",$0
+                        }'
+}
+
+########
+exclude_bashisms()
+{
+    $AWK '{if(index($1,"=(")==0) printf"%s\n",$0}'
+}
+
+########
+write_functions()
+{
+    for f in `$AWK '{if(index($1,"()")!=0) printf"%s\n",$1}' $0`; do
+        sed -n /^$f/,/^}/p $0
+    done
+}
+
+########
+create_script()
+{
+    # Init variables
+    local_name=$1
+    local_command=$2
+
+    # Save previous file (if any)
+    if [ -f ${local_name} ]; then
+        cp ${local_name} ${local_name}.previous
+    fi
+    
+    # Write bash shebang
+    local_BASH_SHEBANG=`init_bash_shebang_var`
+    echo ${local_BASH_SHEBANG} > ${local_name}
+
+    # Write SLURM commands
+    echo "#SBATCH --job-name=${local_command}" >> ${local_name}
+    echo "#SBATCH --output=${local_name}.slurm_out" >> ${local_name}
+
+    # Write environment variables
+    set | exclude_readonly_vars | exclude_bashisms >> ${local_name}
+
+    # Write functions if necessary
+    $GREP "()" ${local_name} -A1 | $GREP "{" > /dev/null || write_functions >> ${local_name}
+    
+    # Write command to be executed
+    echo "${local_command}" >> ${local_name}
+
+    # Give execution permission
+    chmod u+x ${local_name}
+
+    # Archive script with date info
+    curr_date=`date '+%Y_%m_%d'`
+    cp ${local_name} ${local_name}.${curr_date}
+}
+
+########
+launch()
+{
+    local_file=$1
+    local_cpus=$2
+    local_mem=$3
+    local_time=$4
+    
+    if [ -z "${SBATCH}" ]; then
+        $local_file
+    else
+        $SBATCH --cpus-per-task=${local_cpus} --mem=${local_mem} --time ${local_time} $local_file
+    fi
+}
+
+########
+entry_is_ok()
+{
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{if(NF>=4) print"yes\n"; else print"no\n"}'
+}
+
+########
+extract_stepname_from_entry()
+{
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $1}'
+}
+
+########
+extract_cpus_from_entry()
+{
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $2}'
+}
+
+########
+extract_mem_from_entry()
+{
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $3}'
+}
+
+########
+extract_time_from_entry()
+{
+    local_entry=$1
+    echo "${local_entry}" | $AWK '{print $4}'
+}
+
+########
+get_step_dirname()
+{
+    local_dirname=$1
+    local_stepname=$2
+    echo ${local_dirname}/${local_stepname}
+}
+
+########
+reset_outdir_for_step() 
+{
+    local_dirname=$1
+    local_stepname=$2
+    local_outd=`get_step_dirname ${local_dirname} ${local_stepname}`
+
+    if [ -d ${local_outd} ]; then
+        echo "Warning: ${local_stepname} output directory already exists but analysis was not finished, directory content will be removed">&2
+        rm -rf ${local_outd}/* || { echo "Error! could not clear output directory" >&2; return 1; }
+    else
+        mkdir ${local_outd} || { echo "Error! cannot create output directory" >&2; return 1; }
+    fi
+}
+
+########
+execute_step()
+{
+    # Initialize variables
+    local_dirname=$1
+    local_stepname=$2
+    local_cpus=$3
+    local_mem=$4
+    local_time=$5
+
+    # Execute step
+    create_script ${local_dirname}/scripts/execute_${local_stepname} execute_${local_stepname}
+    status=`${bindir}/get_analysis_status -d ${local_dirname} -s "${local_stepname}"`
+    echo "STEP: ${local_stepname} ; STATUS: ${status}" >&2
+    if [ "$status" != "FINISHED" ]; then
+        reset_outdir_for_step ${local_dirname} ${local_stepname} || exit 1
+        launch ${local_dirname}/scripts/execute_${local_stepname} ${local_cpus} ${local_mem} ${local_time}
+    fi
+}
+
+########
+get_step_status()
+{
+    local_dirname=$1
+    local_stepname=$2
+    local_stepdirname=`get_step_dirname ${local_dirname} ${local_stepname}`
+    
+    if [ -d ${local_stepdirname} ]; then
+        if [ -f ${local_stepdirname}/finished ]; then
+            echo "FINISHED"
+        else
+            echo "UNFINISHED"
+        fi
+    else
+        echo "TO-DO"
+    fi
+}

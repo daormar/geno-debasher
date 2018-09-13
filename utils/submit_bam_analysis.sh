096922b87ea63774c79b99aa7147c16552dd79b9
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2018-09-13 11:17:10 +0200

    Redistribute code.

--- utils/submit_bam_analysis.sh
+++ utils/submit_bam_analysis.sh
@@ -260,34 +260,6 @@ get_pars_manta_somatic()
     echo "$ref $normalbam $tumorbam $outd $cpus"
 }
 
-########
-execute_manta_somatic()
-{
-    # Initialize variables
-    local_ref=$1
-    local_normalbam=$2
-    local_tumorbam=$3
-    local_outd=$4
-    local_cpus=$5
-    
-    # Initialize variables
-    MANTA_OUTD=`get_step_dirname ${local_outd} manta_somatic`
-
-    # Activate conda environment
-    conda activate manta
-    
-    # Configure Manta
-    configManta.py --normalBam ${local_normalbam} --tumorBam ${local_tumorbam} --referenceFasta ${local_ref} --runDir ${MANTA_OUTD} > ${MANTA_OUTD}/configManta.log 2>&1 || exit 1
-
-    # Execute Manta
-    ${MANTA_OUTD}/runWorkflow.py -m local -j ${local_cpus} > ${MANTA_OUTD}/runWorkflow.log 2>&1 || exit 1
-
-    # Deactivate conda environment
-    conda deactivate
-
-    # Create file indicating that execution was finished
-    touch ${MANTA_OUTD}/finished
-}
 
 ########
 get_pars_strelka_somatic()
@@ -296,110 +268,11 @@ get_pars_strelka_somatic()
 }
 
 ########
-execute_strelka_somatic()
-{
-    # Initialize variables
-    local_ref=$1
-    local_normalbam=$2
-    local_tumorbam=$3
-    local_outd=$4
-    local_cpus=$5
-
-    # Initialize variables
-    STRELKA_OUTD=`get_step_dirname ${outd} strelka_somatic`
-
-    # Activate conda environment
-    conda activate strelka
-
-    # Configure Strelka
-    configureStrelkaSomaticWorkflow.py --normalBam ${local_normalbam} --tumorBam ${local_tumorbam} --referenceFasta ${local_ref} --runDir ${STRELKA_OUTD} > ${STRELKA_OUTD}/configureStrelkaSomaticWorkflow.log 2>&1 || exit 1
-
-    # Execute Strelka
-    ${STRELKA_OUTD}/runWorkflow.py -m local -j ${local_cpus} > ${STRELKA_OUTD}/runWorkflow.log 2>&1 || exit 1
-
-    # Deactivate conda environment
-    conda deactivate
-
-    # Create file indicating that execution was finished
-    touch ${STRELKA_OUTD}/finished
-}
-
-########
 get_pars_msisensor()
 {
     echo "$ref $normalbam $tumorbam $outd $cpus"
 }
 
-########
-execute_msisensor()
-{
-    # Initialize variables
-    local_ref=$1
-    local_normalbam=$2
-    local_tumorbam=$3
-    local_outd=$4
-    local_cpus=$5
-
-    # Initialize variables
-    MSISENSOR_OUTD=`get_step_dirname ${outd} msisensor`
-    
-    # Activate conda environment
-    conda activate msisensor
-
-    # Create homopolymer and microsatellites file
-    msisensor scan -d ${local_ref} -o ${MSISENSOR_OUTD}/msisensor.list > ${MSISENSOR_OUTD}/msisensor_scan.log 2>&1 || exit 1
-
-    # Run MSIsensor analysis
-    msisensor msi -d ${MSISENSOR_OUTD}/msisensor.list -n ${local_normalbam} -t ${local_tumorbam} -o ${MSISENSOR_OUTD}/output -l 1 -q 1 -b ${local_cpus} > ${MSISENSOR_OUTD}/msisensor_msi.log 2>&1 || exit 1
-
-    # Deactivate conda environment
-    conda deactivate
-    
-    # Create file indicating that execution was finished
-    touch ${MSISENSOR_OUTD}/finished
-}
-
-########
-execute_platypus_germline_conda()
-{
-    # Initialize variables
-    local_ref=$1
-    local_normalbam=$2
-    local_outd=$3
-
-    # Initialize variables
-    PLATYPUS_OUTD=`get_step_dirname ${outd} platypus_germline_conda`
-
-    # Activate conda environment
-    conda activate platypus
-
-    # Run Platypus
-    Platypus.py callVariants --bamFiles=${local_normalbam} --refFile=${local_ref} --output=${PLATYPUS_OUTD}/output.vcf --verbosity=1 > ${PLATYPUS_OUTD}/platypus.log 2>&1 || exit 1
-
-    # Deactivate conda environment
-    conda deactivate
-
-    # Create file indicating that execution was finished
-    touch ${PLATYPUS_OUTD}/finished        
-}
-
-########
-execute_platypus_germline_local()
-{
-    # Initialize variables
-    local_ref=$1
-    local_normalbam=$2
-    local_outd=$3
-
-    # Initialize variables
-    PLATYPUS_OUTD=`get_step_dirname ${outd} platypus_germline_local`
-    
-    # Run Platypus
-    python ${PLATYPUS_BUILD_DIR}/bin/Platypus.py callVariants --bamFiles=${local_normalbam} --refFile=${local_ref} --output=${PLATYPUS_OUTD}/output.vcf --verbosity=1 > ${PLATYPUS_OUTD}/platypus.log 2>&1 || exit 1
-
-    # Create file indicating that execution was finished
-    touch ${PLATYPUS_OUTD}/finished    
-}
 
 ########
 get_pars_platypus_germline()
@@ -407,51 +280,11 @@ get_pars_platypus_germline()
     echo "$ref $normalbam $outd"
 }
 
-########
-execute_platypus_germline()
-{
-    # Initialize variables
-    local_ref=$1
-    local_normalbam=$2
-    local_outd=$3
-
-    if [ -z "${PLATYPUS_BUILD_DIR}" ]; then
-        execute_platypus_germline_conda ${local_ref} ${local_normalbam} ${local_outd}
-    else
-        execute_platypus_germline_local ${local_ref} ${local_normalbam} ${local_outd}
-    fi
-}
 
 ########
 get_pars_cnvkit()
 {
     echo "$ref $normalbam $tumorbam $outd $cpus"
-}
-
-########
-execute_cnvkit()
-{
-    # Initialize variables
-    local_ref=$1
-    local_normalbam=$2
-    local_tumorbam=$3
-    local_outd=$4
-    local_cpus=$5
-
-    # Initialize variables
-    CNVKIT_OUTD=`get_step_dirname ${outd} cnvkit`
-    
-    # Activate conda environment
-    conda activate cnvkit
-
-    # Run cnvkit
-    cnvkit.py batch ${local_tumorbam} -n ${local_normalbam} -m wgs -f ${local_ref}  -d ${CNVKIT_OUTD} -p ${local_cpus} > ${CNVKIT_OUTD}/cnvkit.log 2>&1 || exit 1
-
-    # Deactivate conda environment
-    conda deactivate
-
-    # Create file indicating that execution was finished
-    touch ${CNVKIT_OUTD}/finished
 }
 
 ########
@@ -461,49 +294,9 @@ get_pars_download_ega_norm_bam()
 }
 
 ########
-execute_download_ega_norm_bam()
-{
-    # Initialize variables
-    local_normalbam=$1
-    local_egaid_normalbam=$2
-    local_egastr=$3
-    local_egacred=$4
-    local_outd=$5
-
-    # Initialize variables
-    DOWNLOAD_EGA_NORM_BAM_OUTD=`get_step_dirname ${local_outd} download_ega_norm_bam`
-
-    # Download file
-    ${PYEGA_BUILD_DIR}/pyega3 -c ${local_egastr} -cf ${local_egacred} fetch ${local_egaid_normalbam} ${local_normalbam} > ${DOWNLOAD_EGA_NORM_BAM_OUTD}/pyega3.log 2>&1 || exit 1
-
-    # Create file indicating that execution was finished
-    touch ${DOWNLOAD_EGA_NORM_BAM_OUTD}/finished
-}
-
-########
 get_pars_download_ega_tum_bam()
 {
     echo "$tumorbam ${egaid_tumorbam} $egastr $egacred $outd"
-}
-
-########
-execute_download_ega_tum_bam()
-{
-    # Initialize variables
-    local_tumorbam=$1
-    local_egaid_tumorbam=$2
-    local_egastr=$3
-    local_egacred=$4
-    local_outd=$5
-
-    # Initialize variables
-    DOWNLOAD_EGA_TUM_BAM_OUTD=`get_step_dirname ${outd} download_ega_tum_bam`
-
-    # Download file
-    ${PYEGA_BUILD_DIR}/pyega3 -c ${local_egastr} -cf ${local_egacred} fetch ${local_egaid_tumorbam} ${local_tumorbam} > ${DOWNLOAD_EGA_TUM_BAM_OUTD}/pyega3.log 2>&1 || exit 1
-
-    # Create file indicating that execution was finished
-    touch ${DOWNLOAD_EGA_TUM_BAM_OUTD}/finished
 }
 
 ########
@@ -513,90 +306,15 @@ get_pars_index_norm_bam()
 }
 
 ########
-execute_index_norm_bam()
-{
-    # Initialize variables
-    local_normalbam=$1
-    local_outd=$2
-
-    # Initialize variables
-    INDEX_NORM_BAM_OUTD=`get_step_dirname ${outd} index_norm_bam`
-
-    # Index normal bam file if necessary
-    if [ ! -f ${local_normalbam}.bai ]; then
-
-        # Activate conda environment
-        conda activate base
-
-        # Execute samtools
-        samtools index ${local_normalbam} > ${INDEX_NORM_BAM_OUTD}/samtools.log 2>&1 || exit 1
-
-        # Deactivate conda environment
-        conda deactivate
-    fi
-    
-    # Create file indicating that execution was finished
-    touch ${INDEX_NORM_BAM_OUTD}/finished
-}
-
-########
 get_pars_index_tum_bam()
 {
     echo "$tumorbam $outd"
 }
 
 ########
-execute_index_tum_bam()
-{
-    # Initialize variables
-    local_tumorbam=$1
-    local_outd=$2
-
-    # Initialize variables
-    INDEX_TUM_BAM_OUTD=`get_step_dirname ${outd} index_tum_bam`
-
-    # Index tumor bam file if necessary
-    if [ ! -f ${local_tumorbam}.bai ]; then
-
-        # Activate conda environment
-        conda activate base
-
-        # Execute samtools
-        samtools index ${local_tumorbam} > ${INDEX_TUM_BAM_OUTD}/samtools.log 2>&1 || exit 1
-
-        # Deactivate conda environment
-        conda deactivate
-    fi
-    
-    # Create file indicating that execution was finished
-    touch ${INDEX_TUM_BAM_OUTD}/finished
-}
-
-########
 get_pars_delete_bam_files()
 {
     echo "$normalbam $tumorbam $outd"
-}
-
-########
-execute_delete_bam_files()
-{
-    # Initialize variables
-    local_normalbam=$1
-    local_tumorbam=$2
-    local_outd=$3
-
-    # Initialize variables
-    DELETE_BAM_FILES_OUTD=`get_step_dirname ${outd} delete_bam_files`
-
-    # Delete normal bam file
-    rm ${local_normalbam} > ${DELETE_BAM_FILES_OUTD}/rm_norm.log 2>&1 || exit 1
-    
-    # Delete tumor bam file
-    rm ${local_tumorbam} > ${DELETE_BAM_FILES_OUTD}/rm_tum.log 2>&1 || exit 1
-
-    # Create file indicating that execution was finished
-    touch ${DELETE_BAM_FILES_OUTD}/finished
 }
 
 ########

c6c6dc13fc47aa8a1d4cf31a7e951ed45c0ef1f2
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2018-10-31 10:46:16 +0100

    Add support for Aspera box data.

--- utils/query_ega_metadata.py
+++ utils/query_ega_metadata.py
@@ -32,10 +32,11 @@ def take_pars():
     flags["a_given"]=False
     flags["t_given"]=False
     flags["f_given"]=False
+    flags["p_given"]=False
     values["verbose"]=True
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"s:a:t:f:v",["sampleinfofile=","analysisinfofile=","studyinfofile=","format="])
+        opts, args = getopt.getopt(sys.argv[1:],"s:a:t:f:p:v",["sampleinfofile=","analysisinfofile=","studyinfofile=","format=","asperacontent="])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
@@ -56,6 +57,9 @@ def take_pars():
             if opt in ("-f", "--format"):
                 values["format"] = int(arg)
                 flags["f_given"]=True
+            if opt in ("-p", "--asperacontent"):
+                values["asperacontent"] = arg
+                flags["p_given"]=True
             elif opt in ("-v", "--verbose"):
                 verbose=1
     return (flags,values)
@@ -80,16 +84,19 @@ def check_pars(flags,values):
 
 ##################################################
 def print_help():
-    print >> sys.stderr, "query_ega_metadata -s <string> -a <string> -t <string> -f <int> [-v]"
+    print >> sys.stderr, "query_ega_metadata -s <string> -a <string> -t <string> -f <int>"
+    print >> sys.stderr, "                   [-p <string>] [-v]"
     print >> sys.stderr, ""
     print >> sys.stderr, "-s <string>    File with sample information"
     print >> sys.stderr, "-a <string>    File with analysis information"
     print >> sys.stderr, "-t <string>    File with study information"
     print >> sys.stderr, "-f <int>       Output format:"
-    print >> sys.stderr, "                1: SAMPLE_ACCESSION EGA_SAMPLE_ID FILE_ACCESSION FILENAME DONOR_ID PHENOTYPE GENDER"
+    print >> sys.stderr, "                1: SAMPLE_ACCESSION EGA_SAMPLE_ID FILE_ACCESSION FILENAME ASPERA_BOX_FILENAME DONOR_ID PHENOTYPE GENDER"
     print >> sys.stderr, "                2: Same as 1 but sorted by donor_id"
     print >> sys.stderr, "                3: Same as 2 but entries for same donor_id appear in same line"
-    print >> sys.stderr, "                4: Same as 3 but only EGA_SAMPLE_ID, PHENOTYPE and GENDER are listed"    
+    print >> sys.stderr, "                4: Same as 3 but only EGA_SAMPLE_ID, PHENOTYPE and GENDER are listed"
+    print >> sys.stderr, "                5: Same as 3 but only ASPERA_BOX_FILENAME, PHENOTYPE and GENDER are listed (-p option is required)"
+    print >> sys.stderr, "-p <string>    File listing Aspera box content"
     print >> sys.stderr, "-v             Verbose mode"
 
 ##################################################
@@ -153,7 +160,26 @@ def extract_study_info(filename):
     return study_info_map
 
 ##################################################
-def get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map):
+def load_aspera_box_info(filename):
+    aspera_box_info=[]
+    file = open(filename, 'r')
+    # read file line by line
+    for line in file:
+        line=line.strip("\n")
+        fields=line.split()
+        aspera_box_info.append(fields[0])
+    return aspera_box_info
+
+##################################################
+def match_aspera_filename(egafilename,aspera_box_info):
+    matching = [s for s in aspera_box_info if egafilename in s]
+    if(matching):
+        return matching[0]
+    else:
+        return "NO_ASPERA_FILENAME_AVAILABLE"
+
+##################################################
+def get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map,aspera_box_info):
     formatted_info=[]
 
     # Populate formatted_info structure
@@ -164,7 +190,8 @@ def get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map):
         donor_id=analysis_info_map[ega_sample_id].donor_id
         phenotype=analysis_info_map[ega_sample_id].phenotype
         gender=analysis_info_map[ega_sample_id].gender
-        formatted_info.append((sample_accession,ega_sample_id,fileaccession,filename,donor_id,phenotype,gender))
+        aspera_filename=match_aspera_filename(filename,aspera_box_info)
+        formatted_info.append((sample_accession,ega_sample_id,fileaccession,filename,aspera_filename,donor_id,phenotype,gender))
 
     return formatted_info
 
@@ -183,11 +210,12 @@ def group_formatted_info_by_donor(formatted_info,field_list):
     # Create and populate map to make grouping easier
     group_map={}
     for elem in formatted_info:
-        if(elem[4] in group_map):
-            group_map[elem[4]].append(filter_fields(elem,field_list))
+        donor_id_idx=5
+        if(elem[donor_id_idx] in group_map):
+            group_map[elem[donor_id_idx]].append(filter_fields(elem,field_list))
         else:
-            group_map[elem[4]]=[]
-            group_map[elem[4]].append(filter_fields(elem,field_list))
+            group_map[elem[donor_id_idx]]=[]
+            group_map[elem[donor_id_idx]].append(filter_fields(elem,field_list))
     # Created grouped info
     formatted_info_grouped=[]
     for key in group_map:
@@ -202,18 +230,21 @@ def group_formatted_info_by_donor(formatted_info,field_list):
     return formatted_info_grouped
 
 ##################################################
-def format_info(format,sample_info_map,analysis_info_map,study_info_map):
+def format_info(format,sample_info_map,analysis_info_map,study_info_map,aspera_box_info):
     if(format==1):
-        return get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map)
+        return get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map,aspera_box_info)
     elif(format==2):
-        formatted_info=get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map)
+        formatted_info=get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map,aspera_box_info)
         return sorted(formatted_info, key=operator.itemgetter(4))
     elif(format==3):
-        formatted_info=get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map)
+        formatted_info=get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map,aspera_box_info)
         return group_formatted_info_by_donor(formatted_info,[])
     elif(format==4):
-        formatted_info=get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map)
-        return group_formatted_info_by_donor(formatted_info,[2,5,6])
+        formatted_info=get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map,aspera_box_info)
+        return group_formatted_info_by_donor(formatted_info,[2,6,7])
+    elif(format==5):
+        formatted_info=get_info_in_basic_format(sample_info_map,analysis_info_map,study_info_map,aspera_box_info)
+        return group_formatted_info_by_donor(formatted_info,[4,6,7])
 
 ##################################################
 def print_info(formatted_info):
@@ -232,9 +263,13 @@ def process_pars(flags,values):
     sample_info_map=extract_sample_info(values["sampleinfofile"])
     analysis_info_map=extract_analysis_info(values["analysisinfofile"])
     study_info_map=extract_study_info(values["studyinfofile"])
-
+    if(flags["p_given"]):
+        aspera_box_info=load_aspera_box_info(values["asperacontent"])
+    else:
+        aspera_box_info=[]
+        
     # Format information
-    formatted_info=format_info(values["format"],sample_info_map,analysis_info_map,study_info_map)
+    formatted_info=format_info(values["format"],sample_info_map,analysis_info_map,study_info_map,aspera_box_info)
     
     # Print information
     print_info(formatted_info)

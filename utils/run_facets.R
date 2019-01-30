67ffde396b8b980fbbac21e1f3bbfa5855b64188
jespinosa <jose.espinosa@irbbarcelona.org>
2019-01-30 11:51:41 +0100

    Figures save on the output folder correctly

--- utils/run_facets.R
+++ utils/run_facets.R
@@ -52,14 +52,13 @@ global_model_stats <- data.frame(fitted_model$loglik,
                                  fitted_model$dipLogR)
 
 colnames(global_model_stats) <- c("loglik", "purity", "ploidy", "dipLogR")
-write.csv(global_model_stats, paste0(o, "facets_glob_mdl_stats.csv", row.names = F)
+write.csv(global_model_stats, paste0(o, "/facets_glob_mdl_stats.csv"), row.names = F)
 
 ## Tbl with segmentation results
-write.csv(fitted_model$cncf, paste0(o, "facets_segmetation.csv", row.names = F)
+write.csv(fitted_model$cncf, paste0(o, "/facets_segmetation.csv"), row.names = F)
 
 ## Create facets plot
-# png(paste0(o, "facets_plot.png") width = 900, height = 900, units = "px", pointsize = 18)
-png("facets_plot.png", width = 900, height = 900, units = "px", pointsize = 18)
+png(paste0(o, "/facets_plot.png"), width = 900, height = 900, units = "px", pointsize = 18)
+# png("facets_plot.png", width = 900, height = 900, units = "px", pointsize = 18)
 plotSample(x = oo, emfit = fitted_model)
 dev.off()
-

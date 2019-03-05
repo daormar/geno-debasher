36898a8dcffe85d924098ce97074b089c146053a
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2019-03-05 17:42:24 +0100

    Add new pipeline step.

--- utils/bam_analysis.sh
+++ utils/bam_analysis.sh
@@ -4219,3 +4219,44 @@ delete_bam_files()
 
     display_end_step_message
 }
+
+########
+clear_datadir_explain_cmdline_opts()
+{
+    :
+}
+
+########
+clear_datadir_define_opts()
+{
+    # Initialize variables
+    local cmdline=$1
+    local stepspec=$2
+    local optlist=""
+
+    # Define the -step-outd option, the output directory for the step
+    local step_outd=`get_step_outdir_given_stepspec "$stepspec"`
+    define_opt "-step-outd" ${step_outd} optlist || exit 1
+
+    # -datadir option
+    abs_datadir=`get_absolute_shdirname ${DATADIR_BASENAME}`
+    define_opt "-datadir" ${abs_datadir} optlist || exit 1
+
+    # Save option list
+    save_opt_list optlist
+}
+
+########
+clear_datadir()
+{
+    display_begin_step_message
+
+    # Initialize variables
+    local step_outd=`read_opt_value_from_line "$*" "-step-outd"`
+    local abs_datadir=`read_opt_value_from_line "$*" "-datadir"`
+
+    # Delete bam files
+    rm -rf ${abs_datadir}/*
+
+    display_end_step_message
+}

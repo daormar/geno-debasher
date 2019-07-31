0893f02fac61799e2081fa6912a3616077978cb3
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2019-07-31 09:32:34 +0200

    Add --i option.

--- utils/map_contnames.py
+++ utils/map_contnames.py
@@ -10,9 +10,10 @@ def take_pars():
     flags["m_given"]=False
     flags["f_given"]=False
     flags["c_given"]=False
+    flags["invert"]=False
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"m:f:c:",["mapf=","file=","col="])
+        opts, args = getopt.getopt(sys.argv[1:],"m:f:c:i",["mapf=","file=","col=","invert"])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
@@ -30,6 +31,8 @@ def take_pars():
             elif opt in ("-c", "--col"):
                 values["col"] = int(arg)
                 flags["f_given"]=True
+            elif opt in ("-i", "--invert"):
+                flags["invert"]=True
     return (flags,values)
 
 ##################################################
@@ -48,27 +51,36 @@ def check_pars(flags,values):
 
 ##################################################
 def print_help():
-    print >> sys.stderr, "map_contnames -m <string> -f <string> -c <int>"
+    print >> sys.stderr, "map_contnames -m <string> -f <string> -c <int> [--invert]"
     print >> sys.stderr, ""
     print >> sys.stderr, "-m <string>    File with contig to contig mapping"
     print >> sys.stderr, "-f <string>    File where mapping needs to be done"
     print >> sys.stderr, "-c <int>       Column to be mapped (numbered from 0)"
+    print >> sys.stderr, "--invert       Invert mapping"   
 
 ##################################################
 def get_contig_map(mapf):
     file = open(mapf, 'r')
     contig_map={}
+    inv_contig_map={}
     for line in file:
         line=line.strip("\n")
         fields=line.split()
         if(len(fields)==2):
             contig_map[fields[0]]=fields[1]
-    return contig_map
+            inv_contig_map[fields[1]]=fields[0]
+    return contig_map,inv_contig_map
 
 ##################################################
-def map_contigs(filename,contig_map,col):
-    file = open(filename, 'r')
+def map_contigs(filename,contig_map,inv_contig_map,col,invert):
+    # Select contig map to be used
+    if invert:
+        selected_contig_map=inv_contig_map
+    else:
+        selected_contig_map=contig_map
+
     # read file line by line
+    file = open(filename, 'r')
     for line in file:
         line=line.strip("\n")
         sniffer = csv.Sniffer()
@@ -76,11 +88,11 @@ def map_contigs(filename,contig_map,col):
         fields=line.split(dialect.delimiter)
         if col<len(fields):
             contig=fields[col]
-            if(contig in contig_map):
+            if(contig in selected_contig_map):
                 modified_line=fields[0]
                 for i in range(1,len(fields)):
                     if i==col:
-                        modified_line=modified_line+dialect.delimiter+contig_map[contig]
+                        modified_line=modified_line+dialect.delimiter+selected_contig_map[contig]
                     else:
                         modified_line=modified_line+dialect.delimiter+fields[i]
                 print modified_line
@@ -89,8 +101,8 @@ def map_contigs(filename,contig_map,col):
 
 ##################################################
 def process_pars(flags,values):
-    contig_map=get_contig_map(values["mapf"])
-    map_contigs(values["file"],contig_map,values["col"])
+    contig_map,inv_contig_map=get_contig_map(values["mapf"])
+    map_contigs(values["file"],contig_map,inv_contig_map,values["col"],flags["invert"])
                                    
 ##################################################
 def main(argv):

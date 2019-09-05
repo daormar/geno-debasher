daf29a39d62480656df35189937a0facf54e2811
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2019-09-05 13:09:25 +0200

    Add -k option and rename old -c option.

--- utils/filter_contig_from_genref.py
+++ utils/filter_contig_from_genref.py
@@ -8,11 +8,12 @@ def take_pars():
     flags={}
     values={}
     flags["g_given"]=False
-    flags["c_given"]=False
+    flags["r_given"]=False
+    flags["k_given"]=False
     flags["l_given"]=False
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"g:c:l:",["genref=","contig=","listc="])
+        opts, args = getopt.getopt(sys.argv[1:],"g:r:k:l:",["genref=","contigrem=","contigkeep","listc="])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
@@ -24,9 +25,12 @@ def take_pars():
             if opt in ("-g", "--genref"):
                 values["genref"] = arg
                 flags["g_given"]=True
-            elif opt in ("-c", "--contig"):
-                values["contig"] = arg
-                flags["c_given"]=True
+            elif opt in ("-r", "--contigrem"):
+                values["contigrem"] = arg
+                flags["r_given"]=True
+            elif opt in ("-k", "--contigkeep"):
+                values["contigkeep"] = arg
+                flags["k_given"]=True
             elif opt in ("-l", "--listc"):
                 values["listc"] = arg
                 flags["l_given"]=True
@@ -37,20 +41,31 @@ def check_pars(flags,values):
     if(flags["g_given"]==False):
         print >> sys.stderr, "Error! -g parameter not given"
         sys.exit(2)
-    if(flags["c_given"]==False and flags["l_given"]==False):
-        print >> sys.stderr, "Error! -c or -l parameter not given"
+
+    # Check mutually exclusive options
+    num_simul=0
+    if(flags["r_given"]):
+        num_simul = num_simul + 1
+    if(flags["k_given"]):
+        num_simul = num_simul + 1
+    if(flags["l_given"]):
+        num_simul = num_simul + 1
+
+    if num_simul==0:
+        print >> sys.stderr, "Error! -r, -k or -l parameter not given"
         sys.exit(2)
-    if(flags["c_given"]==True and flags["l_given"]==True):
-        print >> sys.stderr, "Error! -c and -l parameters cannot be given simultaneously"
+    elif num_simul>1:
+        print >> sys.stderr, "Error! -r, -k or -l cannot be given simultaneously"
         sys.exit(2)
 
 ##################################################
 def print_help():
-    print >> sys.stderr, "filter_contig_from_genref -g <string> {-c <string> | -l <string>}"
+    print >> sys.stderr, "filter_contig_from_genref -g <string> {-r <string> | -k <string> | -l <string>}"
     print >> sys.stderr, ""
     print >> sys.stderr, "-g <string>    File with genome reference"
-    print >> sys.stderr, "-c <string>    Name of contig to remove"
-    print >> sys.stderr, "-l <string>    List of contigs to keep (one contig name per line)"
+    print >> sys.stderr, "-r <string>    Name of contig to remove"
+    print >> sys.stderr, "-k <string>    Name of contig to keep"
+    print >> sys.stderr, "-l <string>    File with list of contigs to keep (one contig name per line)"
 
 ##################################################
 def getContigsToKeep(listc):
@@ -65,13 +80,15 @@ def getContigsToKeep(listc):
 ##################################################
 def process_pars(flags,values):
     # Initialize variables
-    if(flags["c_given"]):
-        contigToFilter=values["contig"]
+    if(flags["r_given"]):
+        contigToFilter=values["contigrem"]
     else:
         contigToFilter=""
         
     if(flags["l_given"]):
         contigsToKeep=getContigsToKeep(values["listc"])
+    elif(flags["k_given"]):
+        contigsToKeep=values["contigkeep"]
     else:
         contigsToKeep={}
 
@@ -83,6 +100,7 @@ def process_pars(flags,values):
         line=line.strip("\n")
         fields=line.split()
         if(">" in fields[0]):
+            print line
             contigname=fields[0][1:]
             if(contigname==contigToFilter or (not contigname in contigsToKeep)):
                 skip=True

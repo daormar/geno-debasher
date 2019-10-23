f71c77b2498e85b6ce6fe86ce26580e94d1c82ae
Daniel Ortiz <daniel.ortiz@irbbarcelona.org>
2019-10-23 12:41:39 +0200

    Reorder code.

--- utils/bam_download_steps.sh
+++ utils/bam_download_steps.sh
@@ -313,6 +313,229 @@ download_ega_tum_bam_conda_envs()
 }
 
 ########
+find_bam_filename()
+{
+    local step_outd=$1
+    local result=""
+    
+    for f in ${step_outd}/*.bam; do
+        if [ -f $f ]; then
+            result=$f
+        fi
+    done
+
+    echo ${result}
+}
+
+########
+download_ega_asp_norm_bam_explain_cmdline_opts()
+{
+    # -extn option
+    description="External database id of normal bam file to download"
+    explain_cmdline_req_opt "-extn" "<string>" "$description"
+
+    # -asperausr option
+    description="Username for Aspera server"
+    explain_cmdline_req_opt "-asperausr" "<string>" "$description"
+
+    # -asperapwd option
+    description="Password for Aspera server"
+    explain_cmdline_req_opt "-asperapwd" "<string>" "$description"
+
+    # -asperaserv option
+    description="Name of Aspera server"
+    explain_cmdline_req_opt "-asperaserv" "<string>" "$description"
+
+    # -egadecrpwd option
+    description="File with EGA decryptor password"
+    explain_cmdline_req_opt "-egadecrpwd" "<string>" "$description"
+    
+    # -nt option
+    description="Number of download tries per file (${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} by default)"
+    explain_cmdline_opt "-nt" "<int>" "$description"
+}
+
+########
+download_ega_asp_norm_bam_define_opts()
+{
+    # Initialize variables
+    local cmdline=$1
+    local stepspec=$2
+    local optlist=""
+
+    # Define the -step-outd option, the output directory for the step
+    local step_outd=`get_step_outdir_given_stepspec "$stepspec"`
+    define_opt "-step-outd" ${step_outd} optlist || exit 1
+    
+    # -extn option
+    define_cmdline_opt "$cmdline" "-extn" optlist || exit 1
+
+    # -asperausr option
+    define_cmdline_opt "$cmdline" "-asperausr" optlist || exit 1
+
+    # -asperapwd option
+    define_cmdline_opt "$cmdline" "-asperapwd" optlist || exit 1
+
+    # -asperaserv option
+    define_cmdline_opt "$cmdline" "-asperaserv" optlist || exit 1
+
+    # -egadecrpwd option
+    define_cmdline_opt "$cmdline" "-egadecrpwd" optlist || exit 1
+
+    # -nt option
+    define_cmdline_nonmandatory_opt "$cmdline" "-nt" ${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} optlist || exit 1
+
+    # -normalbam option
+    local abs_datadir=`get_absolute_shdirname ${DATADIR_BASENAME}`
+    local normalbam=${abs_datadir}/normal.bam
+    define_opt "-normalbam" $normalbam optlist || exit 1
+
+    # Save option list
+    save_opt_list optlist
+}
+
+########
+download_ega_asp_norm_bam()
+{
+    # Initialize variables
+    local normalbam=`read_opt_value_from_line "$*" "-normalbam"`
+    local normalbam_file=`read_opt_value_from_line "$*" "-extn"`
+    local aspera_user=`read_opt_value_from_line "$*" "-asperausr"`
+    local aspera_passwd=`read_opt_value_from_line "$*" "-asperapwd"`
+    local aspera_server=`read_opt_value_from_line "$*" "-asperaserv"`
+    local egadecrypt_pwd=`read_opt_value_from_line "$*" "-egadecrpwd"`
+    local download_tries=`read_opt_value_from_line "$*" "-nt"`
+    local step_outd=`read_opt_value_from_line "$*" "-step-outd"`
+    local max_trans_rate=${DEFAULT_ASP_MAX_TRANS_RATE}
+    
+    # Download file
+    logmsg "* Executing ascp (${normalbam_file})..."
+    ASPERA_SCP_PASS=${aspera_passwd} ${ASPERA_HOME_DIR}/bin/ascp --ignore-host-key -QTl ${max_trans_rate} ${aspera_user}@${aspera_server}:${normalbam_file} ${step_outd}/normal.bam.crypt 2>&1 || exit 1
+
+    # Decrypt file
+    logmsg "* Executing decryptor.jar..."
+    $JAVA -jar ${EGADECRYPT_HOME_DIR}/decryptor.jar ${egadecrypt_pwd} ${step_outd}/normal.bam.crypt 2>&1 || exit 1
+    
+    # Obtain file name
+    local bam_file_name=`find_bam_filename ${step_outd}`
+    
+    if [ -z "${bam_file_name}" ]; then
+        logmsg "Error: bam file not found after download process was completed"
+        exit 1
+    fi
+
+    # Move file
+    mv ${bam_file_name} ${normalbam} || exit 1
+
+    # Remove encrypted file
+    rm ${step_outd}/normal.bam.crypt || exit 1
+}
+
+########
+download_ega_asp_tum_bam_explain_cmdline_opts()
+{
+    # -extt option
+    description="External database id of normal bam file to download"
+    explain_cmdline_req_opt "-extt" "<string>" "$description"
+
+    # -asperausr option
+    description="Username for Aspera server"
+    explain_cmdline_req_opt "-asperausr" "<string>" "$description"
+
+    # -asperapwd option
+    description="Password for Aspera server"
+    explain_cmdline_req_opt "-asperapwd" "<string>" "$description"
+
+    # -asperaserv option
+    description="Name of Aspera server"
+    explain_cmdline_req_opt "-asperaserv" "<string>" "$description"
+
+    # -egadecrpwd option
+    description="File with EGA decryptor password"
+    explain_cmdline_req_opt "-egadecrpwd" "<string>" "$description"
+    
+    # -nt option
+    description="Number of download tries per file (${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} by default)"
+    explain_cmdline_opt "-nt" "<int>" "$description"
+}
+
+########
+download_ega_asp_tum_bam_define_opts()
+{
+    # Initialize variables
+    local cmdline=$1
+    local stepspec=$2
+    local optlist=""
+
+    # Define the -step-outd option, the output directory for the step
+    local step_outd=`get_step_outdir_given_stepspec "$stepspec"`
+    define_opt "-step-outd" ${step_outd} optlist || exit 1
+    
+    # -extt option
+    define_cmdline_opt "$cmdline" "-extt" optlist || exit 1
+
+    # -asperausr option
+    define_cmdline_opt "$cmdline" "-asperausr" optlist || exit 1
+
+    # -asperapwd option
+    define_cmdline_opt "$cmdline" "-asperapwd" optlist || exit 1
+
+    # -asperaserv option
+    define_cmdline_opt "$cmdline" "-asperaserv" optlist || exit 1
+
+    # -egadecrpwd option
+    define_cmdline_infile_opt "$cmdline" "-egadecrpwd" optlist || exit 1
+
+    # -nt option
+    define_cmdline_nonmandatory_opt "$cmdline" "-nt" ${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} optlist || exit 1
+
+    # -tumorbam option
+    local abs_datadir=`get_absolute_shdirname ${DATADIR_BASENAME}`
+    local tumorbam=${abs_datadir}/tumor.bam
+    define_opt "-tumorbam" $tumorbam optlist || exit 1
+
+    # Save option list
+    save_opt_list optlist
+}
+
+########
+download_ega_asp_tum_bam()
+{
+    # Initialize variables
+    local tumorbam=`read_opt_value_from_line "$*" "-tumorbam"`
+    local tumorbam_file=`read_opt_value_from_line "$*" "-extt"`
+    local aspera_user=`read_opt_value_from_line "$*" "-asperausr"`
+    local aspera_passwd=`read_opt_value_from_line "$*" "-asperapwd"`
+    local aspera_server=`read_opt_value_from_line "$*" "-asperaserv"`
+    local egadecrypt_pwd=`read_opt_value_from_line "$*" "-egadecrpwd"`
+    local download_tries=`read_opt_value_from_line "$*" "-nt"`
+    local step_outd=`read_opt_value_from_line "$*" "-step-outd"`
+    local max_trans_rate=${DEFAULT_ASP_MAX_TRANS_RATE}
+
+    # Download file
+    logmsg "* Executing ascp (${tumorbam_file})..."
+    ASPERA_SCP_PASS=${aspera_passwd} ${ASPERA_HOME_DIR}/bin/ascp --ignore-host-key -QTl ${max_trans_rate} ${aspera_user}@${aspera_server}:${tumorbam_file} ${step_outd}/tumor.bam.crypt 2>&1 || exit 1
+
+    # Decrypt file
+    logmsg "* Executing decryptor.jar..."
+    $JAVA -jar ${EGADECRYPT_HOME_DIR}/decryptor.jar ${egadecrypt_pwd} ${step_outd}/tumor.bam.crypt 2>&1 || exit 1
+
+    # Obtain file name
+    local bam_file_name=`find_bam_filename ${step_outd}`
+    
+    if [ -z "${bam_file_name}" ]; then
+        logmsg "Error: bam file not found after download process was completed"
+        exit 1
+    fi
+
+    # Move file
+    mv ${bam_file_name} ${tumorbam} || exit 1
+
+    # Remove encrypted file
+    rm ${step_outd}/tumor.bam.crypt || exit 1
+}
+
+########
 download_aws_norm_bam_explain_cmdline_opts()
 {
     # -extn option
@@ -349,21 +572,6 @@ download_aws_norm_bam_define_opts()
 
     # Save option list
     save_opt_list optlist    
-}
-
-########
-find_bam_filename()
-{
-    local step_outd=$1
-    local result=""
-    
-    for f in ${step_outd}/*.bam; do
-        if [ -f $f ]; then
-            result=$f
-        fi
-    done
-
-    echo ${result}
 }
 
 ########
@@ -581,214 +789,6 @@ download_collab_tum_bam()
 
     # Move file
     mv ${bam_file_name} ${tumorbam} || exit 1
-}
-
-########
-download_ega_asp_norm_bam_explain_cmdline_opts()
-{
-    # -extn option
-    description="External database id of normal bam file to download"
-    explain_cmdline_req_opt "-extn" "<string>" "$description"
-
-    # -asperausr option
-    description="Username for Aspera server"
-    explain_cmdline_req_opt "-asperausr" "<string>" "$description"
-
-    # -asperapwd option
-    description="Password for Aspera server"
-    explain_cmdline_req_opt "-asperapwd" "<string>" "$description"
-
-    # -asperaserv option
-    description="Name of Aspera server"
-    explain_cmdline_req_opt "-asperaserv" "<string>" "$description"
-
-    # -egadecrpwd option
-    description="File with EGA decryptor password"
-    explain_cmdline_req_opt "-egadecrpwd" "<string>" "$description"
-    
-    # -nt option
-    description="Number of download tries per file (${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} by default)"
-    explain_cmdline_opt "-nt" "<int>" "$description"
-}
-
-########
-download_ega_asp_norm_bam_define_opts()
-{
-    # Initialize variables
-    local cmdline=$1
-    local stepspec=$2
-    local optlist=""
-
-    # Define the -step-outd option, the output directory for the step
-    local step_outd=`get_step_outdir_given_stepspec "$stepspec"`
-    define_opt "-step-outd" ${step_outd} optlist || exit 1
-    
-    # -extn option
-    define_cmdline_opt "$cmdline" "-extn" optlist || exit 1
-
-    # -asperausr option
-    define_cmdline_opt "$cmdline" "-asperausr" optlist || exit 1
-
-    # -asperapwd option
-    define_cmdline_opt "$cmdline" "-asperapwd" optlist || exit 1
-
-    # -asperaserv option
-    define_cmdline_opt "$cmdline" "-asperaserv" optlist || exit 1
-
-    # -egadecrpwd option
-    define_cmdline_opt "$cmdline" "-egadecrpwd" optlist || exit 1
-
-    # -nt option
-    define_cmdline_nonmandatory_opt "$cmdline" "-nt" ${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} optlist || exit 1
-
-    # -normalbam option
-    local abs_datadir=`get_absolute_shdirname ${DATADIR_BASENAME}`
-    local normalbam=${abs_datadir}/normal.bam
-    define_opt "-normalbam" $normalbam optlist || exit 1
-
-    # Save option list
-    save_opt_list optlist
-}
-
-########
-download_ega_asp_norm_bam()
-{
-    # Initialize variables
-    local normalbam=`read_opt_value_from_line "$*" "-normalbam"`
-    local normalbam_file=`read_opt_value_from_line "$*" "-extn"`
-    local aspera_user=`read_opt_value_from_line "$*" "-asperausr"`
-    local aspera_passwd=`read_opt_value_from_line "$*" "-asperapwd"`
-    local aspera_server=`read_opt_value_from_line "$*" "-asperaserv"`
-    local egadecrypt_pwd=`read_opt_value_from_line "$*" "-egadecrpwd"`
-    local download_tries=`read_opt_value_from_line "$*" "-nt"`
-    local step_outd=`read_opt_value_from_line "$*" "-step-outd"`
-    local max_trans_rate=${DEFAULT_ASP_MAX_TRANS_RATE}
-    
-    # Download file
-    logmsg "* Executing ascp (${normalbam_file})..."
-    ASPERA_SCP_PASS=${aspera_passwd} ${ASPERA_HOME_DIR}/bin/ascp --ignore-host-key -QTl ${max_trans_rate} ${aspera_user}@${aspera_server}:${normalbam_file} ${step_outd}/normal.bam.crypt 2>&1 || exit 1
-
-    # Decrypt file
-    logmsg "* Executing decryptor.jar..."
-    $JAVA -jar ${EGADECRYPT_HOME_DIR}/decryptor.jar ${egadecrypt_pwd} ${step_outd}/normal.bam.crypt 2>&1 || exit 1
-    
-    # Obtain file name
-    local bam_file_name=`find_bam_filename ${step_outd}`
-    
-    if [ -z "${bam_file_name}" ]; then
-        logmsg "Error: bam file not found after download process was completed"
-        exit 1
-    fi
-
-    # Move file
-    mv ${bam_file_name} ${normalbam} || exit 1
-
-    # Remove encrypted file
-    rm ${step_outd}/normal.bam.crypt || exit 1
-}
-
-########
-download_ega_asp_tum_bam_explain_cmdline_opts()
-{
-    # -extt option
-    description="External database id of normal bam file to download"
-    explain_cmdline_req_opt "-extt" "<string>" "$description"
-
-    # -asperausr option
-    description="Username for Aspera server"
-    explain_cmdline_req_opt "-asperausr" "<string>" "$description"
-
-    # -asperapwd option
-    description="Password for Aspera server"
-    explain_cmdline_req_opt "-asperapwd" "<string>" "$description"
-
-    # -asperaserv option
-    description="Name of Aspera server"
-    explain_cmdline_req_opt "-asperaserv" "<string>" "$description"
-
-    # -egadecrpwd option
-    description="File with EGA decryptor password"
-    explain_cmdline_req_opt "-egadecrpwd" "<string>" "$description"
-    
-    # -nt option
-    description="Number of download tries per file (${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} by default)"
-    explain_cmdline_opt "-nt" "<int>" "$description"
-}
-
-########
-download_ega_asp_tum_bam_define_opts()
-{
-    # Initialize variables
-    local cmdline=$1
-    local stepspec=$2
-    local optlist=""
-
-    # Define the -step-outd option, the output directory for the step
-    local step_outd=`get_step_outdir_given_stepspec "$stepspec"`
-    define_opt "-step-outd" ${step_outd} optlist || exit 1
-    
-    # -extt option
-    define_cmdline_opt "$cmdline" "-extt" optlist || exit 1
-
-    # -asperausr option
-    define_cmdline_opt "$cmdline" "-asperausr" optlist || exit 1
-
-    # -asperapwd option
-    define_cmdline_opt "$cmdline" "-asperapwd" optlist || exit 1
-
-    # -asperaserv option
-    define_cmdline_opt "$cmdline" "-asperaserv" optlist || exit 1
-
-    # -egadecrpwd option
-    define_cmdline_infile_opt "$cmdline" "-egadecrpwd" optlist || exit 1
-
-    # -nt option
-    define_cmdline_nonmandatory_opt "$cmdline" "-nt" ${DEFAULT_NUMBER_OF_DOWNLOAD_TRIES} optlist || exit 1
-
-    # -tumorbam option
-    local abs_datadir=`get_absolute_shdirname ${DATADIR_BASENAME}`
-    local tumorbam=${abs_datadir}/tumor.bam
-    define_opt "-tumorbam" $tumorbam optlist || exit 1
-
-    # Save option list
-    save_opt_list optlist
-}
-
-########
-download_ega_asp_tum_bam()
-{
-    # Initialize variables
-    local tumorbam=`read_opt_value_from_line "$*" "-tumorbam"`
-    local tumorbam_file=`read_opt_value_from_line "$*" "-extt"`
-    local aspera_user=`read_opt_value_from_line "$*" "-asperausr"`
-    local aspera_passwd=`read_opt_value_from_line "$*" "-asperapwd"`
-    local aspera_server=`read_opt_value_from_line "$*" "-asperaserv"`
-    local egadecrypt_pwd=`read_opt_value_from_line "$*" "-egadecrpwd"`
-    local download_tries=`read_opt_value_from_line "$*" "-nt"`
-    local step_outd=`read_opt_value_from_line "$*" "-step-outd"`
-    local max_trans_rate=${DEFAULT_ASP_MAX_TRANS_RATE}
-
-    # Download file
-    logmsg "* Executing ascp (${tumorbam_file})..."
-    ASPERA_SCP_PASS=${aspera_passwd} ${ASPERA_HOME_DIR}/bin/ascp --ignore-host-key -QTl ${max_trans_rate} ${aspera_user}@${aspera_server}:${tumorbam_file} ${step_outd}/tumor.bam.crypt 2>&1 || exit 1
-
-    # Decrypt file
-    logmsg "* Executing decryptor.jar..."
-    $JAVA -jar ${EGADECRYPT_HOME_DIR}/decryptor.jar ${egadecrypt_pwd} ${step_outd}/tumor.bam.crypt 2>&1 || exit 1
-
-    # Obtain file name
-    local bam_file_name=`find_bam_filename ${step_outd}`
-    
-    if [ -z "${bam_file_name}" ]; then
-        logmsg "Error: bam file not found after download process was completed"
-        exit 1
-    fi
-
-    # Move file
-    mv ${bam_file_name} ${tumorbam} || exit 1
-
-    # Remove encrypted file
-    rm ${step_outd}/tumor.bam.crypt || exit 1
 }
 
 ########

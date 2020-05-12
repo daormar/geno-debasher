b1dbb8b628fcfcb899b26a775be8f79f741d271d
daormar <dortiz@prhlt.upv.es>
2020-05-12 20:34:05 +0200

    Remove unnecessary line.

--- utils/file_shuffle.sh
+++ utils/file_shuffle.sh
@@ -14,7 +14,6 @@
 # You should have received a copy of the GNU Lesser General Public License
 # along with this program; If not, see <http://www.gnu.org/licenses/>.
   
-# Author: Daniel Ortiz Mart\'inez
 # *- bash -*
 
 # Randomly reorders the lines of a file given a seed value

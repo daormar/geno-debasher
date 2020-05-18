c37d39ade8e39c4edc789f525b402fb90a04c262
daormar <dortiz@prhlt.upv.es>
2020-05-18 19:08:42 +0200

    Update author information.

--- utils/bam_ascat.sh
+++ utils/bam_ascat.sh
@@ -1,5 +1,6 @@
 # Bio-PanPipe package
-# Copyright (C) 2019,2020 Daniel Ortiz-Mart\'inez
+# Copyright (C) 2019,2020 Daniel Ortiz-Mart\'inez and Jose
+# Espinosa-Carrasco (initial version)
 #  
 # This library is free software; you can redistribute it and/or
 # modify it under the terms of the GNU Lesser General Public License

dc4fd659ad9c85d1e64065f6d987547f15fb7b27
daormar <daniel.ortiz.phd@gmail.com>
2021-07-20 18:29:06 +0200

    Improve code robustness.

--- utils/bam_analysis.sh
+++ utils/bam_analysis.sh
@@ -17,7 +17,7 @@
 # *- bash -*
 
 # INCLUDE BASH LIBRARY
-. ${biopanpipe_bindir}/bam_common_lib || exit 1
+. "${biopanpipe_bindir}"/bam_common_lib || exit 1
 
 #################
 # CFG FUNCTIONS #
@@ -26,9 +26,9 @@
 ########
 bam_analysis_shared_dirs()
 {
-    define_shared_dir ${DATADIR_BASENAME}
-    define_shared_dir ${SUMMARYDIR_BASENAME}
-    define_shared_dir ${GERM_SNVS_SUM_DIR_BASENAME}
+    define_shared_dir "${DATADIR_BASENAME}"
+    define_shared_dir "${SUMMARYDIR_BASENAME}"
+    define_shared_dir "${GERM_SNVS_SUM_DIR_BASENAME}"
 }
 
 ########
@@ -38,9 +38,9 @@ bam_analysis_fifos()
 }
 
 # INCLUDE BASH FILES IMPLEMENTING STEPS
-. ${biopanpipe_bindir}/genref_steps || exit 1
-. ${biopanpipe_bindir}/bam_download_steps || exit 1
-. ${biopanpipe_bindir}/bam_manip_steps || exit 1
-. ${biopanpipe_bindir}/bam_analysis_steps || exit 1
-. ${biopanpipe_bindir}/bam_summariz_steps || exit 1
-. ${biopanpipe_bindir}/cleaning_steps || exit 1
+. "${biopanpipe_bindir}"/genref_steps || exit 1
+. "${biopanpipe_bindir}"/bam_download_steps || exit 1
+. "${biopanpipe_bindir}"/bam_manip_steps || exit 1
+. "${biopanpipe_bindir}"/bam_analysis_steps || exit 1
+. "${biopanpipe_bindir}"/bam_summariz_steps || exit 1
+. "${biopanpipe_bindir}"/cleaning_steps || exit 1

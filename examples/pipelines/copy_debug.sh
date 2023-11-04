3252f953efb0a6fc4f461b02624acc45bf000287
daormar <daniel.ortiz.phd@gmail.com>
2023-11-04 20:37:56 +0100

    Remove dependency information.

--- examples/pipelines/copy_debug.sh
+++ examples/pipelines/copy_debug.sh
@@ -4,9 +4,9 @@ load_panpipe_module "bam_analysis"
 
 copy_debug_pipeline()
 {
-    add_panpipe_process "copy_norm_bam"         "cpus=1  mem=2048    time=24:00:00" "processdeps=none"
-    add_panpipe_process "copy_tum_bam"          "cpus=1  mem=2048    time=24:00:00" "processdeps=none"
-    add_panpipe_process "index_norm_bam"        "cpus=1  mem=1024    time=4:00:00"  "processdeps=afterok:copy_norm_bam"
-    add_panpipe_process "index_tum_bam"         "cpus=1  mem=1024    time=4:00:00"  "processdeps=afterok:copy_tum_bam"
-    add_panpipe_process "create_genref_for_bam" "cpus=1  mem=8G      time=4:00:00"  "processdeps=afterok:index_norm_bam"
+    add_panpipe_process "copy_norm_bam"         "cpus=1  mem=2048    time=24:00:00"
+    add_panpipe_process "copy_tum_bam"          "cpus=1  mem=2048    time=24:00:00"
+    add_panpipe_process "index_norm_bam"        "cpus=1  mem=1024    time=4:00:00"
+    add_panpipe_process "index_tum_bam"         "cpus=1  mem=1024    time=4:00:00"
+    add_panpipe_process "create_genref_for_bam" "cpus=1  mem=8G      time=4:00:00"
 }

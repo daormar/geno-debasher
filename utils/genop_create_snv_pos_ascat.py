9515851d7bb9a051df6294153d845008cbb65bee
daormar <daniel.ortiz.phd@gmail.com>
2023-11-06 19:27:18 +0100

    Rename file.

--- utils/genop_create_snv_pos_ascat.py
+++ utils/genop_create_snv_pos_ascat.py
@@ -1,21 +1,21 @@
 """
 Geno-PanPipe package
 Copyright 2019,2020 Daniel Ortiz-Mart\'inez
- 
+
 This library is free software; you can redistribute it and/or
 modify it under the terms of the GNU Lesser General Public License
 as published by the Free Software Foundation; either version 3
 of the License, or (at your option) any later version.
- 
+
 This program is distributed in the hope that it will be useful,
 but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU Lesser General Public License for more details.
- 
+
 You should have received a copy of the GNU Lesser General Public License
 along with this program; If not, see <http://www.gnu.org/licenses/>.
 """
- 
+
 # *- python -*
 
 # import modules
@@ -71,7 +71,7 @@ def check_pars(flags,values):
 
 ##################################################
 def print_help():
-    print("create_snv_pos_ascat -m <float> -g <int> -v <string> -l <string>", file=sys.stderr)
+    print("genop_create_snv_pos_ascat -m <float> -g <int> -v <string> -l <string>", file=sys.stderr)
     print("", file=sys.stderr)
     print("-m <float>     MAF value", file=sys.stderr)
     print("-g <int>       GAP value", file=sys.stderr)
@@ -135,6 +135,6 @@ def main(argv):
 
     # process parameters
     process_pars(flags,values)
-    
+
 if __name__ == "__main__":
     main(sys.argv)

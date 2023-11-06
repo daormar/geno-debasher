20726b3cf115f588ef5886a1aa0e47713b25bda9
daormar <daniel.ortiz.phd@gmail.com>
2023-11-06 19:29:13 +0100

    Revise code formatting.

--- utils/genop_create_snv_pos_ascat.py
+++ utils/genop_create_snv_pos_ascat.py
@@ -19,53 +19,56 @@ along with this program; If not, see <http://www.gnu.org/licenses/>.
 # *- python -*
 
 # import modules
-import sys, gzip, getopt
+import sys
+import gzip
+import getopt
 
 ##################################################
 def take_pars():
-    flags={}
-    values={}
-    flags["m_given"]=False
-    flags["g_given"]=False
-    flags["v_given"]=False
-    flags["l_given"]=False
+    flags = {}
+    values = {}
+    flags["m_given"] = False
+    flags["g_given"] = False
+    flags["v_given"] = False
+    flags["l_given"] = False
 
     try:
-        opts, args = getopt.getopt(sys.argv[1:],"m:g:v:l:",["maf=","gap=","vcf=","listc="])
+        opts, args = getopt.getopt(sys.argv[1:], "m:g:v:l:", [
+                                   "maf=", "gap=", "vcf=", "listc="])
     except getopt.GetoptError:
         print_help()
         sys.exit(2)
-    if(len(opts)==0):
+    if(len(opts) == 0):
         print_help()
         sys.exit()
     else:
         for opt, arg in opts:
             if opt in ("-m", "--maf"):
                 values["maf"] = float(arg)
-                flags["m_given"]=True
+                flags["m_given"] = True
             elif opt in ("-g", "--gap"):
                 values["gap"] = int(arg)
-                flags["g_given"]=True
+                flags["g_given"] = True
             elif opt in ("-v", "--vcf"):
                 values["vcf"] = arg
-                flags["v_given"]=True
+                flags["v_given"] = True
             elif opt in ("-l", "--listc"):
                 values["listc"] = arg
-                flags["l_given"]=True
-    return (flags,values)
+                flags["l_given"] = True
+    return (flags, values)
 
 ##################################################
-def check_pars(flags,values):
-    if(flags["m_given"]==False):
+def check_pars(flags, values):
+    if(flags["m_given"] == False):
         print("Error! -m parameter not given", file=sys.stderr)
         sys.exit(2)
-    if(flags["g_given"]==False):
+    if(flags["g_given"] == False):
         print("Error! -g parameter not given", file=sys.stderr)
         sys.exit(2)
-    if(flags["v_given"]==False):
+    if(flags["v_given"] == False):
         print("Error! -v parameter not given", file=sys.stderr)
         sys.exit(2)
-    if(flags["l_given"]==False):
+    if(flags["l_given"] == False):
         print("Error! -l parameter not given", file=sys.stderr)
         sys.exit(2)
 
@@ -81,60 +84,61 @@ def print_help():
 ##################################################
 def get_contigs(listc):
     file = open(listc, 'r')
-    contigs={}
+    contigs = {}
     for line in file:
-        line=line.strip("\n")
-        fields=line.split()
-        contigs[fields[0]]=1
+        line = line.strip("\n")
+        fields = line.split()
+        contigs[fields[0]] = 1
     return contigs
 
 ##################################################
-def process_vcf(maf,gap,vcf,contigs):
+def process_vcf(maf, gap, vcf, contigs):
     with gzip.open(vcf, "r") as fi:
-        prev_event_contig=""
+        prev_event_contig = ""
         # Process vcf lines
-        for lin in fi :
+        for lin in fi:
             cols = lin.split("\t")
             if len(cols) == 8 and cols[0] in contigs:
                 # Initialize entry variables
-                entry_contig=cols[0]
-                entry_pos=int(cols[1])
-                entry_id=cols[2]
-                entry_maf=-1
-                entry_is_snv=False
+                entry_contig = cols[0]
+                entry_pos = int(cols[1])
+                entry_id = cols[2]
+                entry_maf = -1
+                entry_is_snv = False
 
                 # Extract info
                 info = cols[7].split(";")
                 for i in info:
                     # Check SNV status
-                    if(i=="TSA=SNV"):
-                        entry_is_snv=True
+                    if(i == "TSA=SNV"):
+                        entry_is_snv = True
                     # Check MAF
                     aux = i.split("=")
                     if(aux[0] == "MAF"):
-                        entry_maf=float(aux[1])
+                        entry_maf = float(aux[1])
 
                 # Print data when appropriate
-                if(entry_is_snv and entry_maf >= maf and (prev_event_contig!=entry_contig or (prev_event_contig==entry_contig and entry_pos-gap > prev_event_pos))):
+                if(entry_is_snv and entry_maf >= maf and (prev_event_contig != entry_contig or (prev_event_contig == entry_contig and entry_pos-gap > prev_event_pos))):
                     print("{}\t{}\t{}".format(entry_id, entry_contig, entry_pos))
-                    prev_event_contig=entry_contig
-                    prev_event_pos=entry_pos
+                    prev_event_contig = entry_contig
+                    prev_event_pos = entry_pos
 
 ##################################################
-def process_pars(flags,values):
-    contigs=get_contigs(values["listc"])
-    process_vcf(values["maf"],values["gap"],values["vcf"],contigs)
+def process_pars(flags, values):
+    contigs = get_contigs(values["listc"])
+    process_vcf(values["maf"], values["gap"], values["vcf"], contigs)
 
 ##################################################
 def main(argv):
     # take parameters
-    (flags,values)=take_pars()
+    (flags, values) = take_pars()
 
     # check parameters
-    check_pars(flags,values)
+    check_pars(flags, values)
 
     # process parameters
-    process_pars(flags,values)
+    process_pars(flags, values)
+
 
 if __name__ == "__main__":
     main(sys.argv)

14a04c1c58fa0b1bdbdf984e4da551949c4bfa07
daormar <daniel.ortiz.phd@gmail.com>
2023-11-06 17:58:13 +0100

    Rename file.

--- utils/genop_get_filtered_sam_align.py
+++ utils/genop_get_filtered_sam_align.py
@@ -53,7 +53,7 @@ def check_pars(flags, values):
 
 ##################################################
 def print_help():
-    print("get_filtered_sam_align -l <string>", file=sys.stderr)
+    print("genop_get_filtered_sam_align -l <string>", file=sys.stderr)
     print("", file=sys.stderr)
     print("-l <string>    List of contigs to keep (one contig name per line)", file=sys.stderr)
 
